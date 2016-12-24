87817b4196d979af387de039b03e06ac2f75b0b5
pparonson <preston.aronson@gmail.com>
2016-12-24 07:39:38 -0700

    add Ramda exercises

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,3 +1,4 @@
+console.log( 'starting.. ' );
 /*
  * Javascript: One Parameter Functions
  * write one parameter functions for functionalities that are clearly two
@@ -82,6 +83,7 @@ const arrNames = ["Amitabh Bachchan",
     "Mammootty",
     "Leonardo Di Caprio"
 ];
+// __________________________________________________________________________
 
 /*
  * makeRegexParser takes a regular expression and returns the exec function,
@@ -104,6 +106,7 @@ const validateValueWithFunc = ( value, parseFn, type ) => {
         return `Invalid ${type.toUpperCase()}`;
     }
 };
+// __________________________________________________________________________
 
 /*
  * higher-order function that returns a function:
@@ -120,7 +123,7 @@ const makeAdder = constantValue => {
  * makeAddr returns. That’s because constantValue was in its scope when adder
  * was created.
  */
-
+ // __________________________________________________________________________
 
 const grandParent = (g1, g2) => {
     const g3 = 3;
@@ -137,7 +140,7 @@ const grandParent = (g1, g2) => {
 // const childFunc = parentFunc( 11, 22 ); // returns child()
 // console.log( childFunc( 111, 222 ) ); // prints 738
 // // 1 + 2 + 3 + 11 + 22 + 33 + 111 + 222 + 333 == 738
-
+// __________________________________________________________________________
 
 /*
  * Closures:
@@ -150,6 +153,8 @@ const bankInterest = ( principal, rateOfInterest ) => {
         // Pseudo function
     };
 };
+// __________________________________________________________________________
+
 /*
  * Demonstrate that the inner function has access to the variables of outer
  * function.
@@ -166,6 +171,7 @@ const doMath = ( n1, n2, n3 ) => {
     // return sum without invocation to outer fn doMath
     // return sum;
 };
+// __________________________________________________________________________
 
 /*
  * Demonstrate that closure can have multi level nesting and that the function
@@ -190,6 +196,7 @@ const levelOneFunction = () => {
     // invocation of levelOneFunction returns levelTwoFunction without invocation
     return levelTwoFunction;
 };
+// __________________________________________________________________________
 
 /*
  * Module: a module allows the caller access to multiple functionalities where
@@ -215,6 +222,7 @@ const doMath2 = ( n1, n2 ) => {
     };
     return module;
 };
+// __________________________________________________________________________
 
 /*
  * Modules: similar example that works on strings instead of numbers
@@ -232,6 +240,7 @@ const stringModule = string => {
     };
     return module;
 };
+// __________________________________________________________________________
 
 /*
  * Memoization: technique of keeping handy the results of previous computations
@@ -256,3 +265,384 @@ const memFib = number => {
     };
     return fib( number );
 };
+// __________________________________________________________________________
+
+/*
+ * A simpler counter(as in increment) example which does make use of the fact
+ * that the inner function has access to the outer function variables after the
+ * outer function exits.
+ */
+// const increment = () => {
+//     let _counter = 0;
+//     const increment = () => {
+//         return _counter++;
+//     };
+//     // obj with key: value pairs to expose functionalities
+//     return {
+//         plusOne: increment
+//     };
+//     // return increment;
+// };
+// __________________________________________________________________________
+
+/*
+ * A very similar simple state based example, this time for registry
+ * functionality to collect names that were in attendance.
+ *
+ * The inner function writes into the outer function’s _counter variable.
+ */
+// const attendance = () => {
+//     const _attendees = [];
+//     const _register = name => _attendees.push( name );
+//     // obj with key: value pairs to expose functionalities
+//     return {
+//         attendees: _attendees,
+//         register: _register
+//     };
+// };
+// __________________________________________________________________________
+
+/*
+ * Authentication example: the inner functions use the outer function’s
+ * variables to store state.
+ */
+const authenticate = () => {
+    let _password = 'password';
+    const _login = password => {
+    if ( password === _password ) {
+        return "Logged in successfully";
+    } else {
+        return "Password does not match. Use resetPassword if you forgot";
+    }
+};
+    // resetPassword
+    const _resetPassword = newPassword => {
+        _password = newPassword;
+    };
+    // module
+    return {
+        login: _login,
+        resetPassword: _resetPassword
+    };
+};
+// __________________________________________________________________________
+
+/*
+ * constituency example merely demonstrates that multiple inner functions can
+ * read and write into the same variables. This allows to capture system
+ * behavior where different actors have varying influence on the state.
+ */
+const constituency = () => {
+    let _leftVotes = 0;
+    let _rightVotes = _leftVotes;
+    const _lv3 = () => ++_leftVotes;
+    const _lv2 = _lv3;
+    const _lv1 = _lv2;
+
+    const _rv3 = () => ++_rightVotes;
+    const _rv2 = _rv3;
+    const _rv1 = _rv2;
+
+    // module
+    return {
+        rightVotes: _rightVotes,
+        leftVotes: _leftVotes,
+        leftVoterOne: _lv1,
+        leftVoterTwo: _lv2,
+        leftVoterThree: _lv3,
+        rightVoterOne: _rv1,
+        rightVoterTwo: _rv2,
+        rightVoterThree: _rv3
+    };
+};
+// __________________________________________________________________________
+
+/*
+ * Basically, what we see here is that closure allows functions to retain state
+ * from previous executions. Usually, when a function is invoked, it gets a
+ * fresh start with the values of the parameters passed in and the declared
+ * initial values.
+ */
+const increment = () => {
+    let _counter = 0;
+    const increment = () => {
+        return _counter++;
+    };
+    // obj with key: value pairs to expose functionalities
+    return {
+        plusOne: increment
+    };
+    // return increment;
+};
+// __________________________________________________________________________
+
+/*
+ * Currying or Partial Application: currying refers to the process of adding in
+ * some parameters on to the function to have a modified function that already
+ * has some parameters pre-instated on it and only the remaining parameters need
+ * to be passed in for the function to actually be invoked.
+ */
+
+// 1) First we add currying capability to Function.prototype
+// 2) Next we define a protagonist function
+// 3) After which we create a new version of the protagonist function with a
+//  partial set of arguments curried into the protagonist function.
+// 4) Finally, we validate that the curried version indeed is able to perform
+//  the functionality with just the remaining number of parameters passed in.
+
+// 1) First we add currying capability to Function.prototype
+// Function.prototype.curry = () => {
+//     const curryingFunction = this; // Window object
+//     const curryingArguments = arguments;
+//     const curriedFunction = () => {
+//         const invocationPlusCurriedArguments = [];
+//         curryingArguments.forEach( argument => {
+//             // push each param arg to the new array
+//             invocationPlusCurriedArguments.push( argument );
+//         } );
+//         arguments.forEach( argument => {
+//             invocationPlusCurriedArguments.push( argument );
+//         } );
+//             return curryingFunction().apply( null,
+//                 invocationPlusCurriedArguments);
+//         }; // end curriedFunction
+//     return curriedFunction;
+// };
+
+// 2) Next we define a protagonist function
+const addFiveNumbers = ( n1, n2, n3, n4, n5 ) => {
+    return ( n1 + n2 + n3 + n4 + n5 );
+};
+
+// 3) After which we create a new version of the protagonist function with a
+// partial set of arguments curried into the protagonist function.
+// Invoke currying method on the prototype from the protagonist function to
+// create the curried function.In this example, addTwoNumbers has 1,2,3
+// parameters baked in.
+// const addTwoNumbers = addFiveNumbers.curry( 1, 2, 3 );
+
+/*
+ * Learning JavaScript Closures through the Laws of Karma
+ */
+const sayHi = () => {
+    alert("Yay, I am going to master Closures!");
+};
+const doStuff = functionReference => functionReference();
+
+const getSayHiFunction = () => sayHi;
+// __________________________________________________________________________
+
+/*
+ * You can't directly access "message" variable that's declared inside the test
+ * function from outside the test function
+ */
+const outer = () => {
+    const a = 10;
+    let sum = 0;
+
+    // Nested function definition
+    const inner = () => {
+        const b = 20;
+
+        // Nested function can access outer function's variables and scope
+        sum = a + b;
+    };
+    inner();
+    alert( sum );
+};
+// __________________________________________________________________________
+
+/*
+ * A closure is a name given to a feature in the language by which a nested
+ * function executed after the execution of the outer function can still access
+ * outer function’s scope.
+ */
+const getNewLife = karmaScore => {
+    const message = `My karma score is: ${ karmaScore }`;
+
+    // This is function nesting - declaring a function inside a function.
+    const nextLife = () => {
+        // Please notice the usage of "message" variable
+        console.log( message );
+    };
+
+    // We can return a function from another function
+    return nextLife;
+};
+
+// myNextLife is a function now.
+const myNextLife = getNewLife( 90 );
+
+// alerts "My karma score is: 90".
+// myNextLife();
+
+// myOtherLife is another function now.
+const myOtherLife = getNewLife(95);
+
+// alerts "My karma score is: 95".
+// myOtherLife();
+// __________________________________________________________________________
+
+/*
+ In programming, we often need a sequence generator which generates strings
+ like S1, S2, S3 and so on. Let’s try to solve it with brute force way and see
+ what are the problems with it.
+*/
+let current = 0;
+const getSequenceValue = () => {
+    current = current + 1;
+    return "S" + current;
+};
+
+getSequenceValue(); // S1
+getSequenceValue(); // S2
+
+
+const getSecret = secret => {
+  return {
+    get() {
+        return secret;
+    }
+  };
+};
+
+// secret is bound to a fn that takes an arg 'msg' and returns a fn that returns
+// the arg 'msg'
+const secret = msg => () => msg;
+
+/*
+ Application: The process of applying a function to its arguments in order to
+ produce a return value.
+
+ Partial Application: The process of applying a function to some of its
+ arguments.
+
+ The partially applied function gets returned for later use. In other words, a
+ function that takes a function with multiple parameters and returns a
+ function with fewer parameters.
+*/
+// partialApply is bound to a fn that takes 2 args ( fn, ...fixedArgs) and
+// returns
+const partialApply = ( fn, ...fixedArgs ) => {
+    return function (...remainingArgs) {
+return fn.apply(this, fixedArgs.concat(remainingArgs));
+};
+};
+const add = (a, b) => a + b;
+const add10 = partialApply(add, 10);
+
+/*
+  The function below executes the function bar which was returned
+  when we executed the function foo in the line above. The function bar
+  invokes boop, at which point bar gets suspended and boop gets push
+  onto the top of the call stack (see the screenshot below)
+
+  The fn foo takes an arg "a"; binds 3 const "b", "bar", "boop"; and returns the
+  fn "bar"
+  The const "moar" is bound the the invoked fn foo called with arg 5.
+
+*/
+const x = 10;
+const foo = a => { // a = 5
+    const b = 20;
+
+    const bar = c => { // c = 15
+        const d = 30;
+        return boop( x + a + b + c + d );
+    };
+
+    const boop = e => { // 10 + 5 + 20 + 15 + 30
+        return ( e * -1 );
+    };
+
+    return bar;
+}; // end foo
+
+// const moar = foo(5); // Closure
+
+// moar(15);
+// __________________________________________________________________________
+/*
+ Currying: funfunfunction - Part 6
+*/
+
+// Non-curry version
+let dragon = ( name, size, element ) => {
+    return `${ name } is a ${ size } dragon that breathes ${ element }!`;
+};
+// console.log( `Noncurry: ${ dragon( 'Pete', 'large', 'fire' ) }` );
+
+// Curry version
+let dragonCurry = ( name ) => {
+    return ( size ) => {
+        return ( element) => {
+            return `${ name } is a ${ size } dragon that breathes ${ element }!`;
+        };
+    };
+};
+// console.log( `Curry: ${ dragonCurry( 'Pete' )( 'smaller' )( 'flames' ) }` );
+// __________________________________________________________________________
+/*
+ Practical ramda - functional programming examples:
+ http://www.macwright.org/2015/08/27/practical-ramda.html
+*/
+// console.log( R.prop( 'x', { x: 100 } ) ); //=> 100
+
+let data = [ { id: 'foo' }, { id: 'bar' } ];
+
+// use prop as a fn to compose to map
+const result = R.map( R.prop( 'id' ), data );
+
+// console.log( result );
+/*
+ Thinking in Ramda: Getting Started
+ http://randycoulman.com/blog/2016/05/24/thinking-in-ramda-getting-started/
+*/
+// Replace this:
+const myArray = [ 1, 2, 3, 4, 5, 6 ];
+// for (const value of myArray) {
+//   console.log(value);
+// }
+const logItem = ( ( item ) => {
+	// console.log( item );
+} );
+const logResult = R.forEach( logItem, myArray );
+
+/*
+ filter applies its function (isEven in this case) to each element of the
+ array. Whenever the function returns a “truthy” value, the corresponding
+ element is included in the result. Whenever the function returns a “falsy”
+ value, the corresponding element is excluded (filtered out) from the array.
+*/
+const isEven = ( item ) => item % 2 === 0;
+const filterResult = R.filter( isEven, myArray );
+// console.log( filterResult );
+/*
+ reject does exactly the same thing, but in reverse. It keeps the elements for
+ which the function returns a falsy value and excludes the values for which it
+ returns a truthy value.
+*/
+const isNotEven = ( item ) => item % 2 === 0;
+const rejectResult = R.reject( isEven, myArray );
+// console.log( rejectResult );
+/*
+reduce takes a two-argument function, and initial value, and the array to
+operate on.
+The first argument to the function we pass in is called the “accumulator” and
+the second argument is the value from the array. The function needs to return a
+new accumulator value.
+*/
+const reduceForSum = ( acc, item ) => {
+    return acc + item;
+};
+const reduceResult = R.reduce( reduceForSum, 100, myArray );
+// console.log( reduceResult );
+/*
+ What if we wanted to find the first odd number instead?
+
+ Ramda provides a higher-order function, complement, that takes another function
+ and returns a new function that returns true when the original function returns
+ a falsy value, and false when the original function returns a truthy value.
+*/
+const complementFilterResult = R.filter( R.complement( isEven ), myArray );
+console.log( complementFilterResult );
